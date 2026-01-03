736cead0a6ea52b54ebc658c019665a5c46f79b5
Naresh Babu <jeenepallinaresh429@gmail.com>
2026-01-03 19:49:08 +0530

    Shell scriptinh Practice

---
+++ mongo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/script-logs"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+echo "Script Started executed at : $(date)"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR :: This script should run with root privileges"
+    exit 1
+fi
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+    echo "$2 installation is failed"
+    exit 1
+else
+    echo "Installing $2 is SUCCESS"
+fi
+}
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod 
+VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
